007e995c159589af1b189e255a53314f80342dcb
Jesse C. Fisher <jessecfisher@gmail.com>
2021-02-09 19:22:41 -0800

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_031008) do
+ActiveRecord::Schema.define(version: 2021_02_10_031631) do
 
   create_table "posts", force: :cascade do |t|
     t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "replies", force: :cascade do |t|
+    t.string "name"
+    t.string "body"
+    t.integer "post_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
